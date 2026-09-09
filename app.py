0ab1a8c5584e7219d45f59df0b793eb752c1952e
imoralesarb <imoralesarbelo@gmail.com>
2026-09-09 12:23:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -332,7 +332,7 @@ col0, col_tipo, col1, col2, col3 = st.columns(5)
 with col0:
     filtro_fuente = st.multiselect(
         "🌐 Fuente",
-        ["Licitaciones Generales PLACSP", "Licitaciones Agregadas PLACSP", "TED", "PSCP Catalunya", "Euskadi", "Comunidad de Madrid"],
+        ["Licitaciones Generales PLACSP", "Licitaciones Agregadas PLACSP", "TED", "PSCP Catalunya", "Euskadi", "Comunidad de Madrid", "Contratación Navarra"],
         default=[],
         key="filtro_fuente",
     )
