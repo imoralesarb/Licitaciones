7a0a12dcf51d713f0c3687f2738cf6b192319a08
imoralesarb <imoralesarbelo@gmail.com>
2026-09-09 17:02:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -480,7 +480,9 @@ with col_resultados:
 
         with col_res_texto:
             st.markdown(
-                'Número de resultados:',
+                '<div style="font-size: 13px; padding-top: 8px;">'
+                'Número de resultados:'
+                '</div>',
                 unsafe_allow_html=True
             )
 
