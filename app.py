984860870ce36c252789eaf4d9bec9acd62ca876
imoralesarb <imoralesarbelo@gmail.com>
2026-09-10 10:34:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -544,10 +544,18 @@ def aplicar_filtros_comunes(df):
     #if filtro_fuente:
      #   patron_fuentes = "|".join([r"\b" + f + r"\b" for f in filtro_fuente])
      #   df = df[df["fuente"].str.contains(patron_fuentes, case=False, na=False, regex=True)]
+    # 1. Filtro de fuente
     if filtro_fuente:
-        # Se elimina el uso de \b para evitar conflictos con los espacios en blanco
-        patron_fuentes = "|".join([f for f in filtro_fuente])
-        df = df[df["fuente"].str.contains(patron_fuentes, case=False, na=False, regex=False)]
+        fuentes_seleccionadas = [f.strip().casefold() for f in filtro_fuente]
+    
+        df = df[
+            df["fuente"]
+            .fillna("")
+            .astype(str)
+            .str.strip()
+            .str.casefold()
+            .isin(fuentes_seleccionadas)
+        ]
 
     # 2. Filtro de tipo de contrato flexible
     if filtro_tipo_contrato: 
